d63465348a82db9ac54a9aeb810df86f8a03849e
Dan Buch <dan@meatballhat.com>
2022-06-25 09:01:38 -0400

    Adapt reprex in #1424 for regression test

--- app_regression_test.go
+++ app_regression_test.go
@@ -4,10 +4,10 @@ import (
 	"testing"
 )
 
-// TestRegression tests a regression that was merged between versions 1.20.0 and 1.21.0
+// TestVersionOneTwentyOneRegression tests a regression that was merged between versions 1.20.0 and 1.21.0
 // The included app.Run line worked in 1.20.0, and then was broken in 1.21.0.
 // Relevant PR: https://github.com/urfave/cli/pull/872
-func TestVersionOneTwoOneRegression(t *testing.T) {
+func TestVersionOneTwentyOneRegression(t *testing.T) {
 	testData := []struct {
 		testCase       string
 		appRunInput    []string
@@ -54,6 +54,79 @@ func TestVersionOneTwoOneRegression(t *testing.T) {
 			if err != nil {
 				t.Errorf("did not expected an error, but there was one: %s", err)
 			}
+		})
+	}
+}
+
+// TestSkipArgReorderKnownFlags checks for a bug identified in
+// https://github.com/urfave/cli/issues/1424
+func TestSkipArgReorderKnownFlags(t *testing.T) {
+	for _, tc := range []struct {
+		name           string
+		argv           []string
+		expectedArgs   []string
+		skipArgReorder bool
+	}{
+		{
+			name:         "with_dash_dash",
+			argv:         []string{"1424", "test", "--", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs: []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+		},
+		{
+			name:         "with_dash_dash_including_bool",
+			argv:         []string{"1424", "test", "-t", "--", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs: []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+		},
+		{
+			name:           "with_dash_dash_and_skip_reorder",
+			argv:           []string{"1424", "test", "--", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs:   []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			skipArgReorder: true,
+		},
+		{
+			name:           "with_dash_dash_including_bool_and_skip_reorder",
+			argv:           []string{"1424", "test", "-t", "--", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs:   []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			skipArgReorder: true,
+		},
+		{
+			name:         "without_dash_dash",
+			argv:         []string{"1424", "test", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs: []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+		},
+		{
+			name:         "without_dash_dash_including_bool",
+			argv:         []string{"1424", "test", "-t", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs: []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+		},
+		{
+			name:           "without_dash_dash_and_skip_reorder",
+			argv:           []string{"1424", "test", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs:   []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			skipArgReorder: true,
+		},
+		{
+			name:           "without_dash_dash_including_bool_and_skip_reorder",
+			argv:           []string{"1424", "test", "-t", "zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			expectedArgs:   []string{"zeta", "--beta", "theta", "--alpha", "ori", "--nori", "oin", "--gloin", "-t"},
+			skipArgReorder: true,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			app := NewApp()
+			app.Commands = []Command{{
+				Name:           "test",
+				SkipArgReorder: tc.skipArgReorder,
+				Flags: []Flag{
+					BoolFlag{Name: "t"},
+				},
+				Action: func(c *Context) error {
+					expect(t, c.Args(), Args(tc.expectedArgs))
+					return nil
+				},
+			}}
+
+			expect(t, app.Run(tc.argv), nil)
 		})
 	}
 }
